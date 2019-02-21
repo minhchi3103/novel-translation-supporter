ee2998d154e54e3f78dd90700e2976f9427fc49c
shinryak <minhchi3103@gmail.com>
2019-02-22 01:50:46 +0700

    define some routes

--- src/router.js
+++ src/router.js
@@ -10,11 +10,18 @@ Vue.use(Meta)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'WorkSpace',
       component: WorkSpace
+    },
+    {
+      path: '/work-space',
+      name: 'work-space',
+      component: WorkSpace,
+      children:[{
+        path:'load-old-version',
+        name:'load-from-json-old-version'
+      }]
     },
     {
       path: '/about',
@@ -22,7 +29,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
