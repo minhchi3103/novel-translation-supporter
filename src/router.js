d38981c40afa63a969faec9913048a9f50110344
shinryak <minhchi3103@gmail.com>
2019-02-21 16:42:36 +0700

    use vue-meta plugin
    define custom router component title and meta tag with this plugin

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Meta from 'vue-meta'
+
 import WorkSpace from './views/WorkSpace.vue'
 
 Vue.use(Router)
+Vue.use(Meta)
 
 export default new Router({
   mode: 'history',
