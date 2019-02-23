1496621bd9e6de32585cd06a7be5688fbfa7cd1f
shinryak <minhchi3103@gmail.com>
2019-02-24 04:45:47 +0700

    editable textview component

---
+++ src/helpers/EditText.vue
@@ -0,0 +1,48 @@
+<template>
+  <input v-if="editable==true" type="text" v-model="value" @blur="updateAndExit">
+  <font v-else>
+    {{value}}
+    <font-awesome-icon icon="edit" @click="editable=true" :title="title"/>
+  </font>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      default: "",
+      required: false
+    },
+    title: {
+      default: "edit",
+      required: false
+    }
+  },
+  data() {
+    return {
+      editable: false
+    };
+  },
+  watch: {
+    editable: function() {
+      if (this.editable == true) {
+        this.$nextTick(
+          function() {
+            this.$el.focus();
+          }.bind(this)
+        );
+      }
+    }
+  },
+  methods: {
+    updateAndExit: function() {
+      this.editable = false;
+      this.$emit("input", this.value);
+    }
+  }
+};
+</script>
+<style scoped>
+svg {
+  cursor: pointer;
+}
+</style>
