2c068a390c317ac62bedeef79dca39a716df652b
shinryak <minhchi3103@gmail.com>
2019-02-24 21:40:29 +0700

    build main branch

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,7 @@
 <template>
   <div class="about">
     <div class="container">
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          Dropdown button
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </div>
+      
     </div>
   </div>
 </template>
